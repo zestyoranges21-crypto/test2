e3fd847c7b86fb5ca2066a1c61a8f2fe730528dc
zestyoranges21-crypto <zesty.oranges21@gmail.com>
2026-01-04 10:16:40 +0000

    Add files via upload

---
+++ Greek Values/Untitled-3.cpp
@@ -0,0 +1,13 @@
+
+int main() {
+    cout << "Hello, world!" << endl;
+
+    int a = 5;
+    int b = 7;
+    int sum = a + b;
+
+    cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+
+    return 0;
+}
+
